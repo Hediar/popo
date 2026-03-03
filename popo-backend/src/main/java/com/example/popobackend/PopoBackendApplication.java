2b62fd9edde8ed32e9bcbc7eef118ba1d0b4a902
Hediar <srlimvp@gmail.com>
2026-03-04 00:09:30 +0900

    backend setting

---
+++ popo-backend/src/main/java/com/example/popobackend/PopoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.popobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopoBackendApplication.class, args);
+    }
+
+}
